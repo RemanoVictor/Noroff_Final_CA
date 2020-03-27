8114252d9d6419a7da3a9d496d173eeb5f709257
Remano <remanovictor@icloud.coms>
2020-03-27 22:08:11 +0100

    final CA complete

--- noroff-final-ca/src/components/card-specific.js
+++ noroff-final-ca/src/components/card-specific.js
@@ -2,12 +2,16 @@ import React from "react";
 
 const Cardspecific = props => {
   return (
-    <div className="[ col-sm-8 ] [ card ][ cardspecific ]">
-      <h3> Name: {props.name} </h3>
-      <img src={props.img} alt="playing card" />
-      <p>Colour: {props.colour}</p>
-      <p>Rarity: {props.rarity}</p>
-      <p> About: {props.about}</p>
+    <div className="[ row ][ card ][ cardspecific ]">
+      <div className="[ col-sm-6 ] ">
+        <h2> Name: {props.name} </h2>
+        <img src={props.img} alt="playing card" />
+      </div>
+      <div className="[ col-sm-6 ][ card-specific-body ]">
+        <p>Colour: {props.colour}</p>
+        <p>Rarity: {props.rarity}</p>
+        <p>About: {props.about}</p>
+      </div>
     </div>
   );
 };
--- noroff-final-ca/src/pages/CardSpecific.js
+++ noroff-final-ca/src/pages/CardSpecific.js
@@ -17,26 +17,23 @@ export default function CardSpecific() {
   }, [id]);
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-sm-4"></div>
-        {mtgcharacters !== undefined ? (
-          <Cardspecific
-            id={mtgcharacters.id}
-            name={mtgcharacters.name}
-            img={`${mtgcharacters.imageUrl}`}
-            colour={mtgcharacters.colors[0]}
-            rarity={mtgcharacters.rarity}
-            about={mtgcharacters.text}
+      {mtgcharacters !== undefined ? (
+        <Cardspecific
+          id={mtgcharacters.id}
+          name={mtgcharacters.name}
+          img={`${mtgcharacters.imageUrl}`}
+          colour={mtgcharacters.colors[0]}
+          rarity={mtgcharacters.rarity}
+          about={mtgcharacters.text}
+        />
+      ) : (
+        <div>
+          <img
+            src="https://bloxy.info/assets/progress_horizontal-e1c9f4c66e06ad7aa169dc42e420abe6f097111e9d98cf35dfc162bb41ffffe1.gif"
+            alt="loading"
           />
-        ) : (
-          <div>
-            <img
-              src="https://bloxy.info/assets/progress_horizontal-e1c9f4c66e06ad7aa169dc42e420abe6f097111e9d98cf35dfc162bb41ffffe1.gif"
-              alt="loading"
-            />
-          </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 }
